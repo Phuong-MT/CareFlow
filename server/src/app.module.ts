f241eaaf2b2ee9e0e839a290c0651cb903389dd2
unknown <Phuong.tm225381@sis.hust.edu.vn>
2025-03-12 00:39:23 +0700

    connect database

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -2,11 +2,27 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
+import { SequelizeModule } from '@nestjs/sequelize';
+import { ConfigService } from '@nestjs/config';
 @Module({
   imports: [
     ConfigModule.forRoot({
       envFilePath: `.env.${process.env.NODE_ENV || 'format'}`, 
       isGlobal: true, 
+    }),
+    SequelizeModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => ({
+        dialect: 'mysql',
+        host: configService.get('DB_HOST'),
+        port: +configService.get('DB_PORT'),
+        username: configService.get('DB_USER'),
+        password: configService.get('DB_PASS'),
+        database: configService.get('DB_NAME'),
+        models: [],
+        synchronize: true,
+      }),
     }),
   ],
   controllers: [AppController],
